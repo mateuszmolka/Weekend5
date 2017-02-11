20689c0f939e73fb2da995defda1713b7282eefe
Unknown <b.0910.molmat@gmail.com>
2017-02-11 09:55:35 +0100

    1 & 2

---
+++ js/1-document-ready.js
@@ -0,0 +1,9 @@
+(function toSeconds(seconds){
+    console.log( seconds*1000);
+}(5));
+
+$(function(){
+    'use strict'
+    alert('test');
+    $("#first").css({color: 'red'}).hide('slow').show('fasts');
+})
---
+++ js/2-selectors.js
@@ -0,0 +1,14 @@
+$(function(){
+    'use strict';
+    var paragraphs=$('div>p.paragraf');
+    console.log(paragraphs);
+    
+    var paragrafOne=$('#paragraf');
+    console.log(paragrafOne);
+    
+    var paragrafTwo=$('.paragraf');
+    console.log(paragrafTwo);
+    
+    
+    
+})
