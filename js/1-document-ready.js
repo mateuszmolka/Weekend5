9c8615b08367e843b8f0eff9e57eb2d3cd97c21b
Unknown <b.0910.molmat@gmail.com>
2017-02-12 13:08:12 +0100

    Jquerry

--- js/1-document-ready.js
+++
@@ -1,9 +0,0 @@
-(function toSeconds(seconds){
-    console.log( seconds*1000);
-}(5));
-
-$(function(){
-    'use strict'
-    alert('test');
-    $("#first").css({color: 'red'}).hide('slow').show('fasts');
-})
